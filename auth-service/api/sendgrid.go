d085737b1874d9e5b62d3960eebc205fc1b6d599
Max Ingraham-Rakatansky <maxmir@berkeley.edu>
2020-10-23 02:22:22 -0700

    added sendgrip api key and changed email in sendgrip.go

--- auth-service/api/sendgrid.go
+++ auth-service/api/sendgrid.go
@@ -12,7 +12,7 @@ import (
 var (
 	sendgridKey    string
 	sendgridClient *sendgrid.Client
-	defaultSender  = mail.NewEmail("CloudComputing Decal", "noreply@calcloud.org")
+	defaultSender  = mail.NewEmail("Cloud9 Test", "maxmir@berkeley.edu")
 	defaultScheme  = "http"
 )
 
